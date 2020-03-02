515baa2c9dfa9daa3f956948325e865055534560
suyeoniz <suyeoniz@hanmail.net>
2020-03-02 22:54:37 +0900

    Variable02

---
+++ src/Study_Java03.java
@@ -0,0 +1,17 @@
+//Variable02
+public class Study_Java03 {
+	public static void main(String[] args) {
+		//정수형 데이터 타입 : byte(1),short(2), int(4), long(8)
+				byte b=100;	//1Byte:-128 ~ 127
+				System.out.println("변수b: "+b);
+				
+				short s=10000;	//2byte:-32,768 ~ 32,767
+				System.out.println("변수s: "+s);
+				
+				int i=100000;	//4Byte:-2,147,483,648~2,147,483,647
+				System.out.println("변수i: "+i);
+				
+				long l=1234567890123456789L; //8byte:리터럴(값)뒤에 접미사L을 사용
+				System.out.println("변수l: "+l);
+	}//main()
+}//class
