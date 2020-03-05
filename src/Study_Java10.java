032b52fbad8d87437f601a3cd1c4c711edff5ea2
suyeoniz <suyeoniz@hanmail.net>
2020-03-05 21:23:11 +0900

    Operator07

---
+++ src/Study_Java10.java
@@ -0,0 +1,18 @@
+public class Study_Java10 {
+	public static void main(String[] args) {
+		//2진수 : 0b
+		//8진수 : 0
+		//16진수 : 0x
+		int a = 11;		//10진수
+		System.out.println(a);
+		
+		int b = 0b11;	//2진수
+		System.out.println(b);
+		
+		int c = 011;	//8진수
+		System.out.println(c);
+		
+		int d = 0x11;	//16진수
+		System.out.println(d);
+	}//main
+}//class
