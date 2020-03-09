3bfa91bc04ffa03494be559df191db6a49aec524
suyeoniz <suyeoniz@hanmail.net>
2020-03-09 22:20:34 +0900

    ControlStatement_구구단

---
+++ Study_Java/src/Study_Java29.java
@@ -0,0 +1,27 @@
+public class Study_Java29 {
+	public static void main(String[] args) {
+		for(int i = 2; i <=9; i++) {
+			for(int j = 1; j <= 9; j++) {
+				if(i * j < 10) {
+					System.out.print(i+"X"+j+"=0"+(i*j)+"\t");
+				}else {
+					System.out.print(i+"X"+j+"="+(i*j)+"\t");
+				}//if
+			}//for j
+			System.out.println();
+		}//for i
+
+		System.out.println("=======================================================================");
+		
+		for(int i = 1; i <= 9; i++) {
+			for(int j = 2; j <= 9; j++) {
+				if(i * j <10) {
+					System.out.print(j+"X"+i+"=0"+(i * j)+"\t");
+				}else {
+					System.out.print(j+"X"+i+"="+(i * j)+"\t");
+				}//if
+			}//for j
+			System.out.println();
+		}//for i
+	}//main()
+}//class
