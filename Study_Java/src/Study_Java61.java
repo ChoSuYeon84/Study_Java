0da985b498dc9e549baff91bcbf4033741a3a7f7
suyeoniz <suyeoniz@hanmail.net>
2020-03-16 23:40:45 +0900

    Method_OverLoading

---
+++ Study_Java/src/Study_Java61.java
@@ -0,0 +1,36 @@
+public class Study_Java61 {
+		/*
+		 ★ 메소드 오버로딩(OverLoading)
+		 	- 클래스 안에 같은 이름을 가지고 있는 메소드를 여러개 선언(정의)
+		 	- 매개변수의 개수가 달라야 한다.
+		 	- 매개변수의 개수가 같을 경우에는 매개변수 데이터 타입이 달라야 한다. 
+		 */
+	public static void main(String[] args) {	
+		getSum(10, 20);		//getSum(int, int) 메소드 호출
+		System.out.println("=============");
+		getSum(10, 20, 30);	//getSum(int, int, int)메소드 호출
+		System.out.println("=============");
+		getSum(10.5, 20.3);	//getSum(double, double)메소드 호출
+		System.out.println("=============");
+		getSum(10, 10.5);	//메소드를 만들진 않았지만 자동으로 형변환되어 getSum(double, double)메소드 호출
+	}//main()
+	
+	public static void getSum(int x, int y) {	//getSum(int, int) 메소드 정의
+		System.out.println("첫 번째 인수: "+x);
+		System.out.println("두 번째 인수: "+y);
+		System.out.println("두 인수의 합: "+(x+y));
+	}//getSum()
+	
+	public static void getSum(int x, int y, int z) {	//getSum(int, int, int) 메소드 정의
+		System.out.println("첫 번째 인수: "+x);
+		System.out.println("두 번째 인수: "+y);
+		System.out.println("세 번째 인수: "+z);
+		System.out.println("세 인수의 합: "+(x+y+z));
+	}//getSum()
+	
+	public static void getSum(double x, double y) {	//getSum(double, double) 메소드 정의
+		System.out.println("첫 번째 인수: "+x);
+		System.out.println("두 번째 인수: "+y);
+		System.out.println("두 인수의 합: "+(x+y));		
+	}//getSum()
+}//class
