20f01561204d7882dae77019cfe35f6f1e3c4f75
ChoSuYeon84 <61592472+ChoSuYeon84@users.noreply.github.com>
2020-03-26 23:06:03 +0900

    Update MemberDAO.java

--- Study_Java/src/com/hanul/member/MemberDAO.java
+++ Study_Java/src/com/hanul/member/MemberDAO.java
@@ -15,6 +15,7 @@ public class MemberDAO {
 		System.out.println("===============================================");
 	}//display()
 
+	//나이의 오름차순 정렬
 	public void ageAscSort(ArrayList<MemberDTO> list) {
 		for (int i = 0; i < list.size(); i++) {
 			for (int j = i+1; j < list.size(); j++) {
@@ -28,6 +29,7 @@ public class MemberDAO {
 		System.out.println("===============================================");
 	}//ageAscSort()
 
+	//이름의 내림차순 정렬
 	public void NameDescSort(ArrayList<MemberDTO> list) {
 		for (int i = 0; i < list.size(); i++) {
 			for (int j = i+1; j < list.size(); j++) {
