3410bfae1a2ee032464ca9ce50143400b2b00d2c
suyeoniz <suyeoniz@hanmail.net>
2020-04-15 12:53:31 +0900

    200415_혼공자 연습문제 풀이 (200p 4번)

---
+++ Study_Java/src/HonGonJa4.java
@@ -0,0 +1,14 @@
+public class HonGonJa4 {
+	public static void main(String[] args) {
+		int max = 0;
+		int[] array = {1, 5, 3, 8, 2};
+		
+		for (int i = 0; i < array.length; i++) {
+			if(max < array[i]) {
+				max = array[i];
+			}//if
+		}//for
+		System.out.println("max : "+max);
+		
+	}//main()
+}//class
