8eb9ba3046bfd539b70c0a41fb55d1dd57a987e6
suyeoniz <suyeoniz@hanmail.net>
2020-04-29 21:46:33 +0900

    Ex07

---
+++ Study_Java/src/Ex07.java
@@ -0,0 +1,12 @@
+public class Ex07 {
+	public static void main(String[] args) {
+		//각 데이터형의 크기를 확인하는 프로그램 작성
+		System.out.println("byte 형의 크기 ▶ "+Byte.SIZE);
+		System.out.println("short 형의 크기 ▶ "+Short.SIZE);
+		System.out.println("int 형의 크기 ▶ "+Integer.SIZE);
+		System.out.println("long 형의 크기 ▶ "+Long.SIZE);
+		System.out.println("float 형의 크기 ▶ "+Float.SIZE);
+		System.out.println("double 형의 크기 ▶ "+Double.SIZE);
+		System.out.printf("char 형의 크기 ▶ " +Character.SIZE);
+	}//main
+}//class
